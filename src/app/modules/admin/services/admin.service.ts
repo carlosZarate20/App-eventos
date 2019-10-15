adbd281bf0e00fa02c6ce7f47ef3fb5719f7fe8f
carlosZarate <zaratec31@hotmail.com>
2019-10-15 02:39:16 -0500

    Agregando carpeta de admin

---
+++ src/app/modules/admin/services/admin.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import { environment } from 'src/environments/environment';
+import { ConstantHelper } from 'src/app/Helpers/ConstantsHelpers';
+import * as _ from 'lodash';
+
+@Injectable()
+export class AdminEventService {
+    public headerCustomize: HttpHeaders;
+
+    constructor(private http: HttpClient, private constant: ConstantHelper) {
+        this.headerCustomize = this.constant.getHeaders();
+    }
+
+    findEventSearch(form: any): Observable<Response>{
+        var test = this.constant.getHeaders(true); 
+        return this.http
+        .post('http://edumoreno27-001-site6.etempurl.com/buscar', form)
+        .map((res: Response) => res)
+        .catch(this.handleError);
+    }
+
+    handleError(handleError: any): Observable<Response> {
+        throw new Error('Method not implemented.');
+    }
+}
